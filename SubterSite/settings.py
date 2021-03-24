cb46a4227004946cafb3b269ed9f1db94e0d2af4
Zargerion <iolegx7@gmail.com>
2021-03-24 12:03:09 +0300

    lets start1234567

--- SubterSite/settings.py
+++ SubterSite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '04+fren@e11csoh**1)!ffovbo3d88#uml_)+7zh3@flcwh6m4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['subter.herokuapp.com', '*'] # *
 
