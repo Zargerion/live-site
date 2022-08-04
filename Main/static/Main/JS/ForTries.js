c08bd932c41af387ac71c8f868b4df48ca716328
Zargerion <iolegx7@gmail.com>
2022-08-04 20:21:23 +0300

    maybe

--- Main/static/Main/JS/ForTries.js
+++ Main/static/Main/JS/ForTries.js
@@ -1,5 +1,7 @@
 /*setTimeout(() => { document.getElementById('NeedToHideByJS').style.opacity = 0.75 }, 1000)
-function opacityToZeroTimer() {
+out = setTimeout(() => {document.getElementById('NeedToHideByJS').style.opacity = "0," + String(all_values_of_opacity)}, all_values_of_timers)
+"<br \/>"
+document.getElementById("NeedToDoOutsOfJsHere").innerHTML
 
 }*/
 function jsFunk() {
@@ -7,4 +9,54 @@ function jsFunk() {
     return a
 }
 
-let JS_Try_Mode_Output = jsFunk()
+function minusOpacity() {
+
+    let all_values_of_timers = 0
+    let all_values_of_opacity = 1.00
+    let command_to_decrease_opacity_by_time
+    let d_str = ""
+    let string_of_command
+    let times = 1
+    let govno
+
+    for (let i = 0; i < 20; i++) {
+        /*all_values_of_timers += 175
+        all_values_of_opacity -= 0.05
+        parseFloat( "0." + String(all_values_of_opacity))
+        command_to_decrease_opacity_by_time = setTimeout(() => {document.getElementById('NeedToHideByJS').style.opacity = 1 }, all_values_of_timers)
+        d_str += String(all_values_of_opacity) + " "
+        string_of_command = 'setTimeout(() => {document.getElementById(NeedToHideByJS).style.opacity = ' + String(parseFloat("0." + String(all_values_of_opacity))) + "}, " + String(all_values_of_timers)
+        d_str += string_of_command + "**************" + String(times++) + "**************" */
+    }
+return 1
+}
+
+function stringFromElements() {
+    let str = ''
+    for (let i = 0; i < 20; i++) {
+        str += ' *'
+    }
+return str
+}
+
+    let command_to_decrease_opacity_by_time
+    let all_values_of_timers = 0
+    let all_values_of_opacity = 1.00
+
+    for (let i = 0; i < 20; i++) {
+        command_to_decrease_opacity_by_time = setTimeout(() => {document.getElementById('NeedToHideByJS').style.opacity = console.log(all_values_of_opacity)}, console.log(all_values_of_timers))
+
+
+
+        all_values_of_opacity -= 0.05
+        all_values_of_timers += 175
+    }
+
+
+let js_funk = jsFunk()
+let h = stringFromElements()
+
+
+let JS_Try_Mode_Output = fuck()
+console.log(String(666))
+console.log(fuck())
