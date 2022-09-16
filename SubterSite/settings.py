4b23e3146a995249d04885b4f9f95b0363ebb6b1
Zargerion <iolegx7@gmail.com>
2022-09-17 01:20:28 +0300

    I sdelyat' !!!

--- SubterSite/settings.py
+++ SubterSite/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'Main.context_processors.score',
+                #'Main.context_processors.score',
             ],
         },
     },
